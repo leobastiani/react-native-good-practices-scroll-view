a9149017b07d69c1b2080dfa49c4229520c0716c
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-05-04 13:05:51 -0300

    add properties.json

--- src/commands/read-docs.ts
+++ src/commands/read-docs.ts
@@ -1,9 +1,44 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import {existsSync} from 'node:fs';
 import {readFile, writeFile} from 'node:fs/promises';
 
 (async () => {
   const content = await readDocs();
-  console.log(content);
+  const parsed = content.split('## Methods')[0].split('### `');
+  parsed.shift();
+  const ret = parsed.map(item => {
+    const isBoolean = item.includes('| bool | ');
+    const defaultValueLabel =
+      (() => {
+        return isBoolean
+          ? item.match(/\| bool \| `(\w+)/)?.[1]
+          : item.match(/ \| `'(\w+)'` \|\n/)?.[1];
+      })() ?? '';
+    const only = item.match(/div class="label (\w+)"/)?.[1] ?? '';
+    const values: any = isBoolean
+      ? 'boolean'
+      : item
+          .match(/enum\(([^)]+)\)/)?.[1]
+          .replace(/[`']/g, '')
+          .split(', ') ?? [];
+    const ret = {
+      name: item.split('`')[0],
+    } as any;
+
+    if (defaultValueLabel) {
+      ret.defaultValueLabel = defaultValueLabel;
+    }
+    if (only) {
+      ret.only = only;
+    }
+    if (values.length) {
+      ret.values = values;
+    }
+
+    return ret as unknown;
+  });
+
+  await writeFile('src/properties.json', JSON.stringify(ret, null, 2), 'utf-8');
 })();
 
 async function readDocs() {
